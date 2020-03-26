cda60f42ce27f254430025dd4c7172db9321b338
James Clegg <clobbermouse@gmail.com>
2020-03-26 08:04:25 +0000

    new plots 26th Mar

---
+++ update_repo_and_run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd ~/Documents/covid19/covid19
+ls
+
+#1 go to data repo and get latest data
+cd ~/Documents/covid19/COVID-19
+git status
+git pull -p
+
+git log --oneline -5 > ~/Documents/covid19/covid19/curr_data_master.txt
+
+pwd
+
+#2 go back to main repo and run
+cd ~/Documents/covid19/covid19
+python3 covid19.py
